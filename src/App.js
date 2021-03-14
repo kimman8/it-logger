72c89d3d4e4f5a3787b4ba744391a757c04d2f53
Kim Yuen <kim_man@live.com>
2021-03-15 06:06:33 +1100

    first commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React, { useEffect, Fragment } from "react";
+import "./App.css";
+import "materialize-css/dist/css/materialize.min.css";
+import M from "materialize-css/dist/js/materialize.min.js";
+import SearchBar from "./components/layout/SearchBar";
+import Logs from "./components/logs/Logs";
+import AddBtn from "./components/layout/AddBtn";
+import AddLogModal from "./components/logs/AddLogModal";
+
+const App = () => {
+  useEffect(() => {
+    //initialise materialize JS
+    M.AutoInit();
+  });
+  return (
+    <Fragment>
+      <SearchBar />
+      <div className="container">
+        <AddBtn />
+        <AddLogModal />
+        <Logs />
+      </div>
+    </Fragment>
+  );
+};
+
+export default App;
